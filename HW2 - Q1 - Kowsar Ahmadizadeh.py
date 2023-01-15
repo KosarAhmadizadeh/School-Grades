238ff6bc6578e5ff22bd82b0bcc219d3b217e021
KosarAhmadizadeh <122726651+KosarAhmadizadeh@users.noreply.github.com>
2023-01-15 18:37:07 +0330

    Add files via upload

---
+++ HW2 - Q1 - Kowsar Ahmadizadeh.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+# Kowsar Ahmadizadeh 
+# student number: 40070514
+# Homework 2 - Q1
+
+s = str(input())
+sub = str(input())
+
+def check(s,sub):
+    if (len(s) < len(sub)) or (len(s)==len(sub) and s!= sub) or (sub == "" or s ==""):
+        return ("no")
+    
+    if (s == sub):
+        return ("yes")
+    k=0
+    i=0
+    if len(s)> len(sub):
+        while(i<len(sub)):
+            if s[i]==sub[i]:
+                k+=1
+            i+=1   
+                
+    if k == len(sub):
+        return("yes")
+    if k != len(sub):
+        s=s[1: ]
+        
+    return(check(s,sub))
+            
+
+check(s,sub)                 
+
+
+# In[ ]:
+
+
+
+
